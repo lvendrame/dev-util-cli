0d8d3d2dd5722de69dfc692d6be1aa817543bb80
Luis Fernando Vendrame <lvendrame@gmail.com>
2015-08-20 16:16:40 -0300

    Add new commands

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,8 @@ program
     .version('0.0.1')
     .option('-f, --cpf', 'Select CPF')
     .option('-j, --cnpj', 'Select CNPJ')
+    .option('-n, --name [gender]', 'Select Name')
+    .option('-c, --creditCard', 'Select Credit Card')
     .option('-q, --quantity <number>', 'Quantity to generate', parseInt)
     .option('-m, --mask', 'Add mask to')
     .option('-v, --validate <document>', 'Validate document')
