d769d4b6df913d6b4e32e1e4b44d4f69d9f926ca
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2015-09-03 08:41:32 -0300

    Remove --name feature as it's not working at the moment

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,6 @@ program
     .version('0.0.1')
     .option('-f, --cpf', 'Select CPF')
     .option('-j, --cnpj', 'Select CNPJ')
-    .option('-n, --name [gender]', 'Select Name')
     .option('-c, --creditCard <type>', 'Select Credit Card. Types: maestro, dinersclub, laser, jcb, unionpay, discover, mastercard, amex, visa')
     .option('-q, --quantity <number>', 'Quantity of generate data', parseInt)
     .option('-m, --mask', 'Add mask to')
